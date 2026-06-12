4f2af8954134a2887e17c7d68ee34f31dfe4a1df
Mak100500 <makar020973@gmail.com>
2026-06-12 16:20:29 +0300

    сделал крафты и изменил стату электритовых вещей и брони

--- kubejs/server_scripts/main.js
+++ kubejs/server_scripts/main.js
@@ -1,8 +1,33 @@
 // Visit the wiki for more info - https://kubejs.com
 
+// ====================
+// РЕЦЕПТЫ И КРАФТЫ 
+// ====================
 ServerEvents.recipes(event => {
 
-    
+   
+    const TOOL_KEYS = {
+        E: 'kubejs:electrite_ingot',
+        R: 'minecraft:redstone',
+        Q: 'create:polished_rose_quartz',
+        G: 'create_new_age:overcharged_gold', 
+        S: 'minecraft:stick'
+    }
+
+    const ARMOR_QUARTZ_KEYS = {
+        E: 'kubejs:electrite_ingot',
+        R: 'minecraft:redstone',
+        Q: 'create:polished_rose_quartz',
+        G: 'create_new_age:overcharged_gold'
+    }
+
+    const ARMOR_BASE_KEYS = {
+        E: 'kubejs:electrite_ingot',
+        R: 'minecraft:redstone',
+        G: 'create_new_age:overcharged_gold'
+    }
+
+   
     const steelIngots = [
         'createmetallurgy:steel_ingot', 
         'tfmg:steel_ingot',           
@@ -11,133 +36,112 @@ ServerEvents.recipes(event => {
         'create_ironworks:steel_ingot'
     ]
 
-    
     steelIngots.forEach(steel => {
         if (Item.of(steel).id !== 'minecraft:air') {
-            event.recipes.create.mixing(
-                'kubejs:incomplete_electrite', 
-                [
-                    steel, 
-                    'createaddition:electrum_ingot'
-                ]
-            ).superheated()
+            event.recipes.create.mixing('kubejs:incomplete_electrite', [steel, 'createaddition:electrum_ingot']).superheated()
         }
     })
 
-   
-    event.recipes.create.sequenced_assembly(
-        [
-            'kubejs:electrite_ingot' 
-        ], 
-        'kubejs:incomplete_electrite', 
-        [
-            
-            event.recipes.create.pressing('kubejs:incomplete_electrite', 'kubejs:incomplete_electrite'),
-            
-           
-            event.recipes.create.deploying('kubejs:incomplete_electrite', [
-                'kubejs:incomplete_electrite', 
-                'create_new_age:overcharged_gold'
-            ]),
-            
-            
-            event.recipes.create.deploying('kubejs:incomplete_electrite', [
-                'kubejs:incomplete_electrite', 
-                'minecraft:redstone'
-            ]),
-            
-           
-            event.recipes.create.pressing('kubejs:incomplete_electrite', 'kubejs:incomplete_electrite')
-        ]
-    )
-    .transitionalItem('kubejs:incomplete_electrite') 
-    .loops(1) 
-    .id('kubejs:sequenced_assembly/electrite_ingot')
+    event.recipes.create.sequenced_assembly(['kubejs:electrite_ingot'], 'kubejs:incomplete_electrite', [
+        event.recipes.create.pressing('kubejs:incomplete_electrite', 'kubejs:incomplete_electrite'),
+        event.recipes.create.deploying('kubejs:incomplete_electrite', ['kubejs:incomplete_electrite', 'create_new_age:overcharged_gold']),
+        event.recipes.create.deploying('kubejs:incomplete_electrite', ['kubejs:incomplete_electrite', 'minecraft:redstone']),
+        event.recipes.create.pressing('kubejs:incomplete_electrite', 'kubejs:incomplete_electrite')
+    ]).transitionalItem('kubejs:incomplete_electrite').loops(1).id('kubejs:sequenced_assembly/electrite_ingot')
 
-    // 1. МЕЧ
-    event.recipes.create.sequenced_assembly(
-        ['kubejs:electrite_sword'], 
-        'minecraft:stick', 
-        [
-            event.recipes.create.deploying('kubejs:incomplete_electrite', ['kubejs:incomplete_electrite', 'kubejs:electrite_ingot']),
-            event.recipes.create.deploying('kubejs:incomplete_electrite', ['kubejs:incomplete_electrite', 'kubejs:electrite_ingot']),
-            event.recipes.create.deploying('kubejs:incomplete_electrite', ['kubejs:incomplete_electrite', 'minecraft:redstone']),
-            event.recipes.create.pressing('kubejs:incomplete_electrite', 'kubejs:incomplete_electrite')
-        ]
-    )
-    .transitionalItem('kubejs:incomplete_electrite')
-    .loops(1)
-    .id('kubejs:sequenced_assembly/electrite_sword')
+
+    // ==================================
+    // МЕХАНИЧЕСКИЙ КРАФТ ИНСТРУМЕНТОВ 
+    // ==================================
+
+  // 1. МЕЧ 
+    event.recipes.create.mechanical_crafting('kubejs:electrite_sword', [
+        ' E ',
+        ' E ',
+        'RQR',
+        ' G ',
+        ' S '
+    ], TOOL_KEYS)
 
     // 2. КИРКА
-    event.recipes.create.sequenced_assembly(
-        ['kubejs:electrite_pickaxe'], 
-        'minecraft:stick', 
-        [
-            event.recipes.create.deploying('kubejs:incomplete_electrite', ['kubejs:incomplete_electrite', 'minecraft:stick']),
-            event.recipes.create.deploying('kubejs:incomplete_electrite', ['kubejs:incomplete_electrite', 'kubejs:electrite_ingot']),
-            event.recipes.create.deploying('kubejs:incomplete_electrite', ['kubejs:incomplete_electrite', 'kubejs:electrite_ingot']),
-            event.recipes.create.deploying('kubejs:incomplete_electrite', ['kubejs:incomplete_electrite', 'kubejs:electrite_ingot']),
-            event.recipes.create.deploying('kubejs:incomplete_electrite', ['kubejs:incomplete_electrite', 'minecraft:redstone']),
-            event.recipes.create.pressing('kubejs:incomplete_electrite', 'kubejs:incomplete_electrite')
-        ]
-    )
-    .transitionalItem('kubejs:incomplete_electrite')
-    .loops(1)
-    .id('kubejs:sequenced_assembly/electrite_pickaxe')
+    event.recipes.create.mechanical_crafting('kubejs:electrite_pickaxe', [
+        'EEEEE',
+        'R G R',
+        '  Q  ',
+        '  S  ',
+        '  S  '
+    ], TOOL_KEYS)
 
-    // 3. ТОПОР
-    event.recipes.create.sequenced_assembly(
-        ['kubejs:electrite_axe'], 
-        'minecraft:stick', 
-        [
-            event.recipes.create.deploying('kubejs:incomplete_electrite', ['kubejs:incomplete_electrite', 'minecraft:stick']),
-            event.recipes.create.deploying('kubejs:incomplete_electrite', ['kubejs:incomplete_electrite', 'kubejs:electrite_ingot']),
-            event.recipes.create.deploying('kubejs:incomplete_electrite', ['kubejs:incomplete_electrite', 'kubejs:electrite_ingot']),
-            event.recipes.create.deploying('kubejs:incomplete_electrite', ['kubejs:incomplete_electrite', 'kubejs:electrite_ingot']),
-            event.recipes.create.deploying('kubejs:incomplete_electrite', ['kubejs:incomplete_electrite', 'minecraft:redstone']),
-            event.recipes.create.pressing('kubejs:incomplete_electrite', 'kubejs:incomplete_electrite')
-        ]
-    )
-    .transitionalItem('kubejs:incomplete_electrite')
-    .loops(1)
-    .id('kubejs:sequenced_assembly/electrite_axe')
+    // 3. ТОПОР 
+    event.recipes.create.mechanical_crafting('kubejs:electrite_axe', [
+        'EEE ',
+        'EEQ ',
+        'R S ',
+        'G S ',
+        '  S '
+    ], TOOL_KEYS)
 
     // 4. ЛОПАТА
-    event.recipes.create.sequenced_assembly(
-        ['kubejs:electrite_shovel'], 
-        'minecraft:stick', 
-        [
-            event.recipes.create.deploying('kubejs:incomplete_electrite', ['kubejs:incomplete_electrite', 'minecraft:stick']),
-            event.recipes.create.deploying('kubejs:incomplete_electrite', ['kubejs:incomplete_electrite', 'kubejs:electrite_ingot']),
-            event.recipes.create.deploying('kubejs:incomplete_electrite', ['kubejs:incomplete_electrite', 'minecraft:redstone']),
-            event.recipes.create.pressing('kubejs:incomplete_electrite', 'kubejs:incomplete_electrite')
-        ]
-    )
-    .transitionalItem('kubejs:incomplete_electrite')
-    .loops(1)
-    .id('kubejs:sequenced_assembly/electrite_shovel')
+    event.recipes.create.mechanical_crafting('kubejs:electrite_shovel', [
+        ' E ',
+        'RQR',
+        ' G ',
+        ' S ',
+        ' S '
+    ], TOOL_KEYS)
 
     // 5. МОТЫГА
-    event.recipes.create.sequenced_assembly(
-        ['kubejs:electrite_hoe'], 
-        'minecraft:stick', 
-        [
-            event.recipes.create.deploying('kubejs:incomplete_electrite', ['kubejs:incomplete_electrite', 'minecraft:stick']),
-            event.recipes.create.deploying('kubejs:incomplete_electrite', ['kubejs:incomplete_electrite', 'kubejs:electrite_ingot']),
-            event.recipes.create.deploying('kubejs:incomplete_electrite', ['kubejs:incomplete_electrite', 'kubejs:electrite_ingot']),
-            event.recipes.create.deploying('kubejs:incomplete_electrite', ['kubejs:incomplete_electrite', 'minecraft:redstone']),
-            event.recipes.create.pressing('kubejs:incomplete_electrite', 'kubejs:incomplete_electrite')
-        ]
-    )
-    .transitionalItem('kubejs:incomplete_electrite')
-    .loops(1)
-    .id('kubejs:sequenced_assembly/electrite_hoe')
+    event.recipes.create.mechanical_crafting('kubejs:electrite_hoe', [
+        'EEEE',
+        'R Q ',
+        '  S ',
+        '  G ',
+        '  S '
+    ], TOOL_KEYS)
 
+    // =============================
+    // МЕХАНИЧЕСКИЙ КРАФТ БРОНИ 
+    // =============================
+
+    // ШЛЕМ
+    event.recipes.create.mechanical_crafting('kubejs:electrite_helmet', [
+        'EEEEE',
+        'R Q R',
+        'G   G'
+    ], ARMOR_QUARTZ_KEYS)
+
+    // НАГРУДНИК
+    event.recipes.create.mechanical_crafting('kubejs:electrite_chestplate', [
+        'E   E',
+        'ERERE',
+        'EGQGE',
+        'EEEEE',
+        ' EEE '
+    ], ARMOR_QUARTZ_KEYS)
+
+    // ПОНОЖИ
+    event.recipes.create.mechanical_crafting('kubejs:electrite_leggings', [
+        'EEEEE',
+        'R   R',
+        'G   G',
+        'E   E',
+        'E   E'
+    ], ARMOR_BASE_KEYS)
+
+    // БОТИНКИ
+    event.recipes.create.mechanical_crafting('kubejs:electrite_boots', [
+        'E   E',
+        'R   R',
+        'G   G'
+    ], ARMOR_BASE_KEYS)
 })
 
+// ===========
+// 4. ТЕГИ
+// ===========
 ServerEvents.tags('item', event => {
     event.add('minecraft:cluster_max_harvestables', 'kubejs:electrite_pickaxe')
     event.add('minecraft:picks', 'kubejs:electrite_pickaxe')
     event.add('kubejs:electrite_ingot', 'kubejs:electrite_ingot')
+    event.add('c:ingots/electrite', 'kubejs:electrite_ingot') 
 })
--- kubejs/startup_scripts/main.js
+++ kubejs/startup_scripts/main.js
@@ -2,120 +2,84 @@
 
 Platform.getInfo('kubejs').name = 'Create: Electric Steel'
 
-
+// ==================================
+// 1. РЕГИСТРАЦИЯ МАТЕРИАЛА БРОНИ 
+// ==================================
 StartupEvents.registry('armor_material', event => {
     event.create('electrite')
         .defense({
-            helmet: 4,
-            chestplate: 9,
-            leggings: 7,
-            boots: 4
+            helmet: 6,       
+            chestplate: 12, 
+            leggings: 9,     
+            boots: 5         
         })
-        .toughness(3.0)
-        .knockbackResistance(0.15)
-        .enchantmentValue(24)
+        .toughness(5.0)               
+        .knockbackResistance(0.40)    
+        .enchantmentValue(30)         
         .equipSound('minecraft:item.armor.equip_netherite')
-        .repairIngredient(() => Ingredient.of('#c:ingots/electrite'))
+        .repairIngredient(() => Ingredient.of('#kubejs:electrite_ingot'))
 })
 
-// РЕГИСТРАЦИЯ ВСЕХ ПРЕДМЕТОВ
+// ==========================================
+// 2. РЕГИСТРАЦИЯ ПРЕДМЕТОВ И ИНСТРУМЕНТОВ
+// ==========================================
 StartupEvents.registry('item', event => {
 
     // МАТЕРИАЛЫ
     event.create('electrite_ingot').texture('kubejs:item/electrite_ingot')
     event.create('incomplete_electrite').texture('kubejs:item/incomplete_electrite')
 
+    // ХАРАКТЕРИСТИКИ ИНСТРУМЕНТОВ 
+    const tech = tier => {
+        tier.uses = 3500               
+        tier.speed = 24.0              
+        tier.enchantmentValue = 30     
+        tier.repairIngredient = '#kubejs:electrite_ingot'
+    }
 
-    // ИНСТРУМЕНТЫ
-
-    // МЕЧ
+    // МЕЧ 
     event.create('electrite_sword', 'sword')
         .texture('kubejs:item/electrite_sword')
         .tier('netherite')
-        .attackDamageBaseline(4.0)
-        .speedBaseline(-2.0)
-        .modifyTier(tier => {
-            tier.uses = 1800
-            tier.speed = 16.0
-            tier.enchantmentValue = 25
-            tier.repairIngredient = '#kubejs:electrite_ingot'
-        })
+        .attackDamageBaseline(7.0)     
+        .speedBaseline(-1.8)           
+        .modifyTier(tech)
 
-    // ТОПОР
+    // ТОПОР 
     event.create('electrite_axe', 'axe')
         .texture('kubejs:item/electrite_axe')
         .tier('netherite')
-        .attackDamageBaseline(6.0)
-        .speedBaseline(-2.6)
-        .modifyTier(tier => {
-            tier.uses = 1800
-            tier.speed = 16.0
-            tier.enchantmentValue = 25
-            tier.repairIngredient = '#kubejs:electrite_ingot'
-        })
+        .attackDamageBaseline(10.0)   
+        .speedBaseline(-2.4)
+        .modifyTier(tech)
 
-    // КИРКА
+    // КИРКА 
     event.create('electrite_pickaxe', 'pickaxe')
         .texture('kubejs:item/electrite_pickaxe')
         .tier('netherite')
-        .attackDamageBaseline(2.0)
-        .speedBaseline(-2.4)
-        .modifyTier(tier => {
-            tier.uses = 1800
-            tier.speed = 16.0
-            tier.enchantmentValue = 25
-            tier.repairIngredient = '#kubejs:electrite_ingot'
-        })
+        .attackDamageBaseline(4.0)
+        .speedBaseline(-2.0)
+        .modifyTier(tech)
 
-    // ЛОПАТА
+    // ЛОПАТА 
     event.create('electrite_shovel', 'shovel')
         .texture('kubejs:item/electrite_shovel')
         .tier('netherite')
-        .attackDamageBaseline(2.5)
-        .speedBaseline(-2.6)
-        .modifyTier(tier => {
-            tier.uses = 1800
-            tier.speed = 16.0
-            tier.enchantmentValue = 25
-            tier.repairIngredient = '#kubejs:electrite_ingot'
-        })
+        .attackDamageBaseline(4.5)
+        .speedBaseline(-2.2)
+        .modifyTier(tech)
 
     // МОТЫГА
     event.create('electrite_hoe', 'hoe')
         .texture('kubejs:item/electrite_hoe')
         .tier('netherite')
-        .attackDamageBaseline(-2.0)
-        .speedBaseline(0.5)
-        .modifyTier(tier => {
-            tier.uses = 1800
-            tier.speed = 16.0
-            tier.enchantmentValue = 25
-            tier.repairIngredient = '#kubejs:electrite_ingot'
-        })
+        .attackDamageBaseline(-1.0)
+        .speedBaseline(1.0)
+        .modifyTier(tech)
 
-
-   // БРОНЯ
-event.create('electrite_helmet', 'helmet')
-    .material('kubejs:electrite')
-    .texture('kubejs:item/electrite_helmet')
-    .fireResistant()
-    .maxDamage(420)
-
-event.create('electrite_chestplate', 'chestplate')
-    .material('kubejs:electrite')
-    .texture('kubejs:item/electrite_chestplate')
-    .fireResistant()
-    .maxDamage(600)
-
-event.create('electrite_leggings', 'leggings')
-    .material('kubejs:electrite')
-    .texture('kubejs:item/electrite_leggings')
-    .fireResistant()
-    .maxDamage(560)
-
-event.create('electrite_boots', 'boots')
-    .material('kubejs:electrite')
-    .texture('kubejs:item/electrite_boots')
-    .fireResistant()
-    .maxDamage(490)
+    // БРОНЯ
+    event.create('electrite_helmet', 'helmet').material('kubejs:electrite').texture('kubejs:item/electrite_helmet').fireResistant().maxDamage(650)
+    event.create('electrite_chestplate', 'chestplate').material('kubejs:electrite').texture('kubejs:item/electrite_chestplate').fireResistant().maxDamage(950)
+    event.create('electrite_leggings', 'leggings').material('kubejs:electrite').texture('kubejs:item/electrite_leggings').fireResistant().maxDamage(850)
+    event.create('electrite_boots', 'boots').material('kubejs:electrite').texture('kubejs:item/electrite_boots').fireResistant().maxDamage(750)
 })
